16d16bc634c8397e43ca152bab8a7edaed51d763
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2023-01-29 09:50:51 -0500

    init

---
+++ rectcut-raylib/source/rectcut.d
@@ -0,0 +1,125 @@
+module rectcut;
+import std.algorithm;
+import std.traits;
+
+struct RectCut(T) if (isNumeric!(T))
+{
+    enum Side
+    {
+        left,
+        right,
+        top,
+        bottom
+    }
+
+    T minX, minY, maxX, maxY;
+    
+    auto cut(T a, Side side)
+    {
+        switch(side)
+        {
+            case Side.right: return rect.cutRight(a);
+            case Side.left:  return rect.cutLeft(a);
+            case Side.top:   return rect.cutTop(a);
+            default:         return rect.cutBottom(a);
+        }
+    }
+    
+    auto cutLeft(T a)
+    {
+        T originalX = minX;
+        minX = min(maxX, minX + a);
+        return RectCut(originalX, minY, minX, maxY);
+    }
+    
+    auto cutRight(T a)
+    {
+        T originalX = maxX;
+        maxX = max(minX, maxX - a);
+        return RectCut(maxX, minY, originalX, maxY);
+    }
+    
+    auto cutTop(T a)
+    {
+        T originalY = minY;
+        minY = min(maxX, minY + a);
+        return RectCut(minX, originalY, maxX, minY);
+    }
+    
+    auto cutBottom(T a)
+    {
+        T originalY = maxY;
+        maxY = max(minY, maxY - a);
+        return RectCut(minX, maxY, maxX, originalY);
+    }
+    
+    auto getLeft(T a)
+    {
+        T newX = min(maxX, minX + a);
+        return RectCut(minX, minY, newX, maxY);
+    }
+    
+    auto getRight(T a)
+    {
+        newX = max(minX, maxX - a);
+        return RectCut(newX, minY, maxX, maxY);
+    }
+    
+    auto getTop(T a)
+    {
+        newY = min(maxX, minY + a);
+        return RectCut(minX, minY, maxX, newY);
+    }
+    
+    auto getBottom(T a)
+    {
+        newY = max(minY, maxY - a);
+        return RectCut(minX, newY, maxX, maxY);
+    }
+    
+    auto addLeft(T a)
+    {
+        return RectCut(minX - a, minY, maxX, maxY);
+    }
+    
+    auto addRight(T a)
+    {
+        return RectCut(minX, minY, maxX + a, maxY);
+    }
+    
+    auto addTop(T a)
+    {
+        return RectCut(minX, minY - a, maxX, maxY);
+    }
+    
+    auto addBottom(T a)
+    {
+        return RectCut(minX, minY, maxX, maxY + a);
+    }
+    
+    auto extend(T a)
+    {
+        return RectCut(minX - a, minY - a, maxX + a, maxY + a);
+    }
+    
+    auto contract(T a)
+    {
+        return RectCut(minX + a, minY + a, maxX - a, maxY - a);
+    }
+    
+    void selfExtend(T a)
+    {
+        minX -= a; 
+        minY -= a; 
+        maxX += a; 
+        maxY += a;
+    }
+    
+    void selfcontract(T a)
+    {
+        minX += a; 
+        minY += a; 
+        maxX -= a; 
+        maxY -= a;
+    }
+}
