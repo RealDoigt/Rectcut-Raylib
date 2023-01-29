0c10a09b598e9f0f909f329b8847e45ecd97a159
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2023-01-29 12:09:07 -0500

    Update to_raylib_rect.d

--- source/to_raylib_rect.d
+++ source/to_raylib_rect.d
@@ -2,7 +2,7 @@ module to_raylib_rect;
 import rectcut;
 import raylib;
 
-auto rectcutToRectangle(RectCut!float r)
+auto rectcutToRectangle(RectCut r)
 {
     auto width = r.maxX - r.minX, height = r.maxY - r.minY;
     return Rectangle(r.minX, r.minY, width, height);
@@ -11,5 +11,5 @@ auto rectcutToRectangle(RectCut!float r)
 auto rectangleToRectcut(Rectangle r)
 {
     auto maxX = r.x + r.width, maxY = r.height + r.y;
-    return RectCut!float(r.x, r.y, maxX, maxY);
+    return RectCut(r.x, r.y, maxX, maxY);
 }
